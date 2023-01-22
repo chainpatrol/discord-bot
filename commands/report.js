6d17560424c402b6fa72cf0846da8118ed0037da
Nikita Varabei <nVarabei@gmail.com>
2023-01-22 12:10:29 -0500

    add discord bot files, deploy commands globally when DISCORD_DEPLOY_GLOBAL is set

---
+++ commands/report.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const { SlashCommandBuilder } = require("discord.js");
+
+const data = new SlashCommandBuilder()
+  .setName("report")
+  .setDescription("reports a scam link to ChainPatrol")
+  .addStringOption((option) =>
+    option
+      .setName("url")
+      .setDescription("The scam link to report")
+      .setRequired(true)
+  );
+
+module.exports = {
+  data: data,
+  async execute(interaction) {
+    try {
+      // submit report
+      const url = interaction.options.getString("url");
+      const response = await axios.post(
+        "https://app.chainpatrol.io/api/v2/report",
+        {
+          organizationSlug: "mountaintop",
+          title: "Scam targetting mountaintop with an airdrop",
+          description: "test description",
+          assets: [
+            {
+              content: url,
+              status: "BLOCKED",
+              type: "URL",
+            },
+          ],
+        }
+      );
+
+      await interaction.reply(`Report sent, ID: ${response.data.id}`);
+    } catch (error) {
+      // Handle errors
+      console.error("error", error);
+      await interaction.reply("Error with submitting report");
+    }
+  },
+};
---
+++ deploy-commands.js
@@ -0,0 +1,58 @@
+// Guide: https://discordjs.guide/creating-your-bot/command-deployment.html#command-registration
+const { REST, Routes } = require("discord.js");
+// const { clientId, guildId, token } = require('./config.json');
+const fs = require("node:fs");
+const os = require("os");
+
+const clientId = process.env["DISCORD_APPLICATION_ID"];
+const guildId = process.env["TEST_DISCORD_SERVER_ID"];
+const token = process.env["DISCORD_BOT_SECRET"];
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(token);
+
+// and deploy your commands!
+const deployCommands = async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    if (process.env.DISCORD_DEPLOY_GLOBAL) {
+      await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands across all servers (global).`
+      );
+    } else {
+      // The put method is used to fully refresh all commands in the guild with the current set
+      const data = await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+      );
+
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands on dev server ${guildId}.`
+      );
+    }
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+};
+
+module.exports = {
+  deployCommands,
+};

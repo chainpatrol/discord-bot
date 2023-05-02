f44ab94c87a1a8ef43fba2ef9b04b88202db4fe0
Max <33969759+MaXeraph@users.noreply.github.com>
2023-05-02 01:40:39 -0400

    #19 add more input model report command (#23)
    * add: nodemon to live-reload
    - `dev` command
    
    * add: popup modal with required fields
    - expires after 1 min
    
    * remove: package-lock
    
    * ignore: package-lock
    
    * modify: remove package-lock from gitignore
    - Revert yarn lock to yarn mirror
    
    * modify: using await instead of then/catch
    - some fields set to not required
    
    * fix: 400 error when submitting empty strings array
    - declearative types
    
    * modify:
    - remove screenshot input
    - add discriminator to username
    
    * dev command
    
    ---------
    
    Co-authored-by: Umar Ahmed <umar.ahmed1998@gmail.com>

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  CommandInteraction,
+  ModalBuilder,
+  SlashCommandBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ModalActionRowComponentBuilder,
+  User,
+  CacheType,
+  CommandInteractionOptionResolver,
+} from "discord.js";
 import { env } from "../env";
 
 export const data = new SlashCommandBuilder()
@@ -19,17 +30,32 @@ export async function execute(interaction: CommandInteraction) {
 
   try {
     const { guildId, user, options } = interaction;
-    const url = options.getString("url", true);
+    const modal = generateModal(user, options);
+
+    // Show the modal to the user
+    await interaction.showModal(modal);
+
+    // extract data from modal
+    const submissionResult = await interaction.awaitModalSubmit({
+      time: 60000,
+    });
+
+    const url = submissionResult.fields.getTextInputValue("urlInput");
     const escapedUrl = url.replace(".", "(dot)");
+
+    const title = submissionResult.fields.getTextInputValue("titleInput");
+    const description =
+      submissionResult.fields.getTextInputValue("descriptionInput");
+    const contact = submissionResult.fields.getTextInputValue("contactInput");
 
     // submit report
     const response = await axios.post(
       `${env.CHAINPATROL_API_URL}/api/v2/report/create`,
       {
         discordGuildId: guildId,
-        title: "Discord Report",
-        description: `reported by discord user ${user.username} , Discord ID: ${user.id}`,
-        contactInfo: `discord user ${user.username} , Discord ID: ${user.id}`,
+        title: title,
+        description: description,
+        contactInfo: contact,
         assets: [
           {
             content: url,
@@ -41,7 +67,7 @@ export async function execute(interaction: CommandInteraction) {
       }
     );
 
-    await interaction.reply({
+    await submissionResult.reply({
       content: `✅ Thanks for submitting a report for \`${escapedUrl}\` ! \n\nWe've sent this report to the **${response.data.organization.name}** team and **ChainPatrol** to conduct a review. Once approved the report will be sent out to wallets to block.\n\nThanks for doing your part in making this space safer 🚀`,
       ephemeral: true,
     });
@@ -53,4 +79,83 @@ export async function execute(interaction: CommandInteraction) {
       ephemeral: true,
     });
   }
+}
+
+function generateModal(
+  user: User,
+  options: Omit<
+    CommandInteractionOptionResolver<CacheType>,
+    "getMessage" | "getFocused"
+  >
+) {
+  const usernameWithDiscriminator = `${user.username}#${user.discriminator}`;
+  const url = options.getString("url", true);
+
+  const modal = new ModalBuilder()
+    .setCustomId("reportModal")
+    .setTitle("Report Scam Link");
+
+  const urlInput = new TextInputBuilder()
+    .setCustomId("urlInput")
+    // The label is the prompt the user sees for this input
+    .setLabel("Scam link to be reported")
+    // Short means only a single line of text
+    .setStyle(TextInputStyle.Short)
+    .setValue(url)
+    .setPlaceholder(url);
+  // An action row only holds one text input,
+  // so you need one action row per text input.
+  const urlActionRow =
+    new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+      urlInput
+    );
+
+  const titleInput = new TextInputBuilder()
+    .setCustomId("titleInput")
+    .setLabel("Title")
+    .setStyle(TextInputStyle.Short)
+    .setPlaceholder(`Discord Report: ${url}`)
+    .setValue(`Discord Report: ${url}`);
+  const titleActionRow =
+    new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+      titleInput
+    );
+
+  const descriptionInput = new TextInputBuilder()
+    .setCustomId("descriptionInput")
+    .setLabel("Description")
+    .setRequired(false)
+    .setStyle(TextInputStyle.Paragraph)
+    .setPlaceholder(`Please explain why you think this is a scam`)
+    .setValue(
+      `reported by discord user ${usernameWithDiscriminator} , Discord ID: ${user.id}`
+    );
+  const descripionActionRow =
+    new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+      descriptionInput
+    );
+
+  const contactInput = new TextInputBuilder()
+    .setCustomId("contactInput")
+    .setLabel("Let us know how to best contact you")
+    .setRequired(false)
+    .setStyle(TextInputStyle.Paragraph)
+    .setPlaceholder(`Please provide your Discord username and ID`)
+    .setValue(
+      `discord user ${usernameWithDiscriminator} , Discord ID: ${user.id}`
+    );
+  const contactActionRow =
+    new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+      contactInput
+    );
+
+  // Add inputs to the modal (maximum 5)
+  modal.addComponents(
+    urlActionRow,
+    titleActionRow,
+    descripionActionRow,
+    contactActionRow
+  );
+
+  return modal;
 }

85dda64d0b27f082493c2804d9d3cdf8aaa61500
Max <33969759+MaXeraph@users.noreply.github.com>
2023-05-16 03:21:02 +0000

    add: test envvars

---
+++ .jest/envVars.js
@@ -0,0 +1,7 @@
+process.env.DISCORD_APPLICATION_ID = "123456789012345678";
+process.env.DISCORD_BOT_SECRET = "secret";
+process.env.CHAINPATROL_API_URL = "http://localhost";
+process.env.CHAINPATROL_API_KEY = "secret";
+process.env.TEST_DISCORD_SERVER_ID = "123456789012345678";
+process.env.DISCORD_DEPLOY_GLOBAL = "false";
+process.env.SENTRY_SECRET = "secret";

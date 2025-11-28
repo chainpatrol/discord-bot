66cf897546ee8dea8c62223b23473696c679b130
smuushi <mishih780@gmail.com>
2025-11-28 09:41:13 -0500

    Improve URL extraction to match bare domains without protocol

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,8 +2,16 @@ export function defangUrl(url: string) {
   return url.replaceAll(".", "(dot)");
 }
 
-export const URL_REGEX = /https?:\/\/(?:www\.)?([^\s]+\.[^\s]+)/gi;
+export const URL_REGEX = /(?:https?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/gi;
 
 export function extractUrls(str: string) {
-  return str.match(URL_REGEX);
+  const matches = str.match(URL_REGEX);
+  if (!matches) return null;
+  
+  return matches.map(url => {
+    if (!url.match(/^https?:\/\//i)) {
+      return `https://${url}`;
+    }
+    return url;
+  });
 }

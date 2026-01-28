9e46bd8f87a009671552076d675758c5a27cc641
smuushi <mishih780@gmail.com>
2026-01-28 09:52:15 -0500

    chore: format code

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,13 +2,14 @@ export function defangUrl(url: string) {
   return url.replaceAll(".", "(dot)");
 }
 
-export const URL_REGEX = /(?:https?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/gi;
+export const URL_REGEX =
+  /(?:https?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/gi;
 
 export function extractUrls(str: string) {
   const matches = str.match(URL_REGEX);
   if (!matches) return null;
-  
-  return matches.map(url => {
+
+  return matches.map((url) => {
     if (!url.match(/^https?:\/\//i)) {
       return `https://${url}`;
     }
